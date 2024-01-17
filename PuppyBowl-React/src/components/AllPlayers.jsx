280522eb9bd476ee1da9eeade681264f1b3e1c47
Brandon <146400057+badams242@users.noreply.github.com>
2024-01-17 18:32:02 -0500

    delelted an add on for import App.css

--- PuppyBowl-React/src/components/AllPlayers.jsx
+++ PuppyBowl-React/src/components/AllPlayers.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { fetchPlayers } from '../API';
 
+
 const AllPlayers = () => {
   const [players, setPlayers] = useState([]);
 
